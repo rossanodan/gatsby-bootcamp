7d8eb402f1e0ee57253a2830f5a50c0042776a0d
Rossano-DAngelo <rossano.dangelo@ibm.com>
2019-07-05 00:19:33 +0100

    Added pages, enabled scss modules via gatsby plugin, added navigation

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,14 +3,14 @@ import { Link } from "gatsby";
 
 import Layout from "../components/layout";
 
-const IndexPage = () => {
+const AboutPage = () => {
   return (
     <Layout>
-      <h1>Hello.</h1>
-      <h2>I'm Rossano, a frontend developer living in beautiful Galway.</h2>
+      <h1>About</h1>
+      <p>I'm Rossano, a frontend developer living in beautiful Galway.</p>
       <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
     </Layout>
   )
-};
+}
 
-export default IndexPage;
+export default AboutPage;
---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Layout from "../components/layout";
+
+const ContactPage = () => {
+  return (
+    <Layout>
+      <h1>Contact</h1>
+      <p>You can contact me via <a href="https://twitter.com/RossanoDan" target="_blank">Twitter</a> or via e-mail.</p>
+    </Layout>
+  )
+}
+
+export default ContactPage;
